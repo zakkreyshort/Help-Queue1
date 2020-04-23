16f8b2a695b60f4f976374cae9944a47511b1130
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-23 09:56:27 -0700

    Add component for viewing details of a ticket
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

---
+++ src/components/TicketDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function TicketDetail(props){
+   const { ticket, onClickingDelete } = props;
+  
+  return (
+    <React.Fragment>
+      <h1>Ticket Details</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p>{ticket.issue}</p>
+      <button onClick={()=> onClickingDelete(ticket.id) }>Destroy Ticket</button>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func
+};
+
+export default TicketDetail;
